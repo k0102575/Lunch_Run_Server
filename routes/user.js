5c6e6cbff66b3bbf164a33ff61df22bed608585a
SeungMin Kim <k0102575@gmail.com>
2019-08-05 01:18:22 +0900

    async 추가

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,13 @@
-var express = require('express');
-var router = express.Router();
-const datasource = require('../util/datasource')
-const connection = datasource.getConnection()
+const express = require('express');
+const router = express.Router();
+const datasource = require('../util/datasource');
+const connection = datasource.getConnection();
+const async = require('async');
 
 router.get('/user', function(req, res, next) {
 
     try {
-
-        var stmt = 'select * from user';
-        connection.query(stmt, function (err, result) {
-            console.log(result)
+        connection.query('select * from user', function (err, result) {
             res.status(200).json(result);
         })    
     } catch(e) {
@@ -22,44 +20,43 @@ router.post('/user', function(req, res, next) {
 
     try {
 
-        let body = req.body
+        const {email, phone, password, alias} = req.body
 
-        let email = body.email
-        let phone = body.phone
-
-        connection.query("select * from user where email = ?", email, function (err, result) {
-
-            if(result.length) {
-                res.status(409).json("email");
-            } else {
-                connection.query("select * from user where phone = ?", phone, function (err, result) {
-
+        var tasks = [
+            function (callback) {
+                connection.query("select * from user where email = ?", email, function (err, result) {
                     if(result.length) {
-                        res.status(409).json("phone");
+                        callback(new Error('email'));
                     } else {
-                        connection.query('insert into user (email, password, alias, phone) VALUES(?, password(?), ?, ?)', [email, body.password, body.alias, phone], function(err, rows, fields){
-                            if(err){
-                                res.status(500).json(err);
-                            } else {
-                                res.status(200).json(rows.insertId)
-                            }
-                        });
+                        callback(null);
                     }
-                })    
-                
+                })
+            },
+            function (callback) {
+                connection.query("select * from user where phone = ?", phone, function (err, result) {
+                    if(result.length) {
+                        callback(new Error('phone'));
+                    } else {
+                        callback(null);
+                    }
+                })
             }
-        })    
+        ];
         
-        // connection.query('insert into user (email, password, alias, phone) VALUES(?, password(?), ?, ?)', [email, body.password, body.alias, phone], function(err, rows, fields){
-        //     if(err){
-        //         console.log(err);
-        //         res.status(500).json(err);
-        //     } else {
-        //         console.log(rows)
-        //         res.status(200).json(rows.insertId)
-        //     }
-        // });
-
+        async.series(tasks, function (err, results) {
+            if(!err) {
+                connection.query('insert into user (email, password, alias, phone) VALUES(?, password(?), ?, ?)', [email, password, alias, phone], function(err, rows, fields){
+                    if(err){
+                        res.status(500).json(err);
+                    } else {
+                        res.status(200).json(rows.insertId)
+                    }
+                });
+            } else {
+                res.status(409).json(err.message);
+            }
+        });
+        
     } catch(e) {
         console.log(e)
         res.json(e);
