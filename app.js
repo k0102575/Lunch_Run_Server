fadc3c27ec7132e875c3d1945c2176b5f99b6dd8
Theo.kim <theo.kim@triplllet.com>
2019-08-10 16:23:18 +0900

    swagger init

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerOptions = require("./util/swagger/option.js")
 
-var indexRouter = require('./routes/index');
-var userRouter = require('./routes/user');
-var authRouter = require('./routes/auth');
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
-var app = express();
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/user');
+const authRouter = require('./routes/auth');
+
+const app = express();
 
 // view engine setup
 app.set('views', __dirname + '/views');
@@ -24,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/', userRouter);
 app.use('/auth', authRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ util/swagger/option.js
@@ -0,0 +1,16 @@
+var swaggerDefinition = {
+  info: {
+    title: 'Lunch_run',
+    version: '1.0.0',
+    description: 'Lunch Run Project',
+  },
+  host: '54.180.89.60',
+  basePath: '/'
+}
+
+var options = {
+  swaggerDefinition: swaggerDefinition,
+  apis: ['./util/swagger/doc.yaml'],
+}
+
+module.exports = options
